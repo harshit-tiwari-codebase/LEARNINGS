93d41fe641a83da161a888f43c445466c639e1b2
harshit-tiwari-codebase <harshit20112005@gmail.com>
2026-03-02 23:28:16 +0530

    day 3 : Learned about creating react app using vite

---
+++ React/day 3/practise/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="card">
+        <h1>Hello I am Harshit 👋</h1>
+        <p>Welcome to our React journey 🚀</p>
+        <button>Started Learning</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
