f793faf6e29cd84bb4354a4f190a63d8cf3ff579
harshit-tiwari-codebase <harshit20112005@gmail.com>
2026-05-21 03:01:18 +0530

    feat : created all 4 API's

---
+++ Backend/BACKEND 6/Backend/src/app.js
@@ -0,0 +1,59 @@
+// create a server and configure it
+
+const express = require("express");
+const notemodel = require("./models/notes.model");
+const app = express();
+
+app.use(express.json());
+
+const notes = [];
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+//used to create the data using POST method
+app.post("/api/notespad", async (req, res) => {
+  // notes.push(req.body);
+
+  const { title, description } = req.body;
+  const note = await notemodel.create({ title, description });
+  console.log("note is created");
+
+  res.status(201).json({
+    message: "note created",
+    note,
+  });
+});
+
+//get the data using the api
+app.get("/api/notespad", async (req, res) => {
+  const a = await notemodel.find();
+  res.status(200).json({
+    message: "note created",
+    a,
+  }) / api;
+});
+
+//create the delete data using DELETE method
+app.delete("/api/notespad/:id", async (req, res) => {
+ 
+  await notemodel.findByIdAndDelete(req.params.id);
+
+  console.log(req.params.id);
+
+  res.status(200).json({
+    message: "notes deleted successfully",
+  });
+});
+
+//create the update feature using the PATCH method
+
+app.patch("/api/notespad/:id",async (req, res)=>{
+  const id = req.params.id;
+  const {description} = req.body;
+  await notemodel.findByIdAndUpdate(id,{description})
+  res.status(200).json({message:"notes description is updated successfully"})
+})
+
+module.exports = app;
---
+++ Backend/BACKEND 6/Frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-zinc-700 text-sm'>
+      
+    </div>
+  )
+}
+
+export default App
